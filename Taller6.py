c9566923f2b1585b298bca0b895fdced05221c18
FishHunter1 <102452694+FishHunter1@users.noreply.github.com>
2022-09-08 07:22:13 -0500

    Taller 6.py

--- Taller6.py
+++ Taller6.py
@@ -3,20 +3,20 @@ n=float(input("Escriba un numero en Radianes \n"))
 es=(0.5*pow(10,-8))*100
 interacion=1
 pot=2
-valor=1
+val=1
 signo=-1
 
 while n > es:
     if signo==-1:
         operacion=round((valor+pow(n,pot)) / math.factorial(pot),8)
-        valor=float(operacion)
+        val=float(operacion)
         print(f'Interacion # {interacion}')
         interacion +=1
         pot +=2
         signo *=-1
     else:
         operacion=round((valor-pow(n,pot)) / math.factorial(pot),8)
-        valor=float(operacion)
+        val=float(operacion)
         print(f"Interacion # {interacion}")
         interacion +=1
         pot +=2
