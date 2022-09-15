babd278c6ebe5b2607d803945dc0c7854d61d384
FishHunter1 <102452694+FishHunter1@users.noreply.github.com>
2022-09-15 07:46:06 -0500

    Add files via upload

---
+++ Taller 12.py
@@ -0,0 +1,19 @@
+from math import factorial
+from math import e
+xi=0.55
+xi_1=0.555
+h=0.005
+valor_ant = 0
+operacion = 0
+for i in range(16):
+    if i % 2 ==0:
+        operacion+= (e**(-xi)/factorial(i))*h**i
+    else:
+        operacion -= (e**(-xi)/factorial(i)*h**i)
+
+    ea=abs((operacion-valor_ant)/operacion)*100
+    valor_ant=operacion
+    print("orden",i)
+    print("Resultado",operacion)
+    print("error",ea,"%")
+    
